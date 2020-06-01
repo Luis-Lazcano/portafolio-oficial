190c732155ab7d408709de5359cfe388197dda1f
Luis LC <luis.lazcanocruz@gmail.com>
2020-05-31 22:59:56 -0500

    Updates

---
+++ precache-manifest.b8e59751755845c0da0554f18453692f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b0a5d0397d75fd34aa95747e226514",
+    "url": "/portafolio-oficial/index.html"
+  },
+  {
+    "revision": "bb9ddb4395d23a02e33b",
+    "url": "/portafolio-oficial/static/css/main.43702673.chunk.css"
+  },
+  {
+    "revision": "a9004b8157236104bc7d",
+    "url": "/portafolio-oficial/static/js/2.7aa6d1b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portafolio-oficial/static/js/2.7aa6d1b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb9ddb4395d23a02e33b",
+    "url": "/portafolio-oficial/static/js/main.debcc898.chunk.js"
+  },
+  {
+    "revision": "7d04a1ffb39a6adc5fc0",
+    "url": "/portafolio-oficial/static/js/runtime-main.7ae957bc.js"
+  },
+  {
+    "revision": "89a70b5b4b5451f1b4769a6cbe61ba31",
+    "url": "/portafolio-oficial/static/media/Linkdein@2x.89a70b5b.png"
+  },
+  {
+    "revision": "36b08801a367974cb795f39208b0136e",
+    "url": "/portafolio-oficial/static/media/Profile@2x.36b08801.png"
+  }
+]);
